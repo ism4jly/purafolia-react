98bdccc3784e995ac8f86764eee1be7542f7cd1c
Felipe Lemos <42103234+lemosflp@users.noreply.github.com>
2023-11-02 18:02:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Importe seu arquivo CSS aqui
+
+// Importe os componentes que você criou
+// Em App.tsx
+import Header from './components/Header';
+import Cabecalho from './components/Cabecalho';
+import About from './components/About';
+import Card from './components/Card';
+import Contato from './components/Contato';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Cabecalho />
+      <About />
+      <Card />
+      <Contato />
+      <Footer />
     </div>
   );
 }
